e0157072b6e5fd41b8e4565ede26d680604f7fab
Nguyen Van Minh Tri <mtanime020705@gmail.com>
2024-12-20 08:06:23 +0700

    Add files via upload

--- Css8bai8.cpp
+++ Css8bai8.cpp
@@ -3,21 +3,14 @@ int main(){
 	int matrix[4][4]={{1,2,5,4},{4,2,4,5},{6,7,1,8},{5,2,4,7}};
 	int sum;
 	for(int i=0;i<4;i++){
-	    for(int j=0;j<4;j++){
-	    		sum+=matrix[i][j];
-	    		
-					if(i+j==3){
-						printf("%d  ",matrix[i][j]);
-					}else{
-						printf("*   ");
+					if(matrix[i][4-i-1]){
+						printf("%d ",matrix[i][4-i-1]);
+						sum+=matrix[i][4-i-1];
 					}
-	    			
-				
+					
 				}
-			printf("\n\n");}
-	
-
-    printf("sum index of matrix is %d",sum);
-    return 0;
+				
+				 printf("sum index of matrix is %d",sum);
+		return 0;
  }
  //[0;3],[1;2],[2;1],[3]
