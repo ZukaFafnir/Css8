cb962edce67188b5460e09e570fd0fa9f4d3ef8d
Nguyen Van Minh Tri <mtanime020705@gmail.com>
2024-12-20 07:53:21 +0700

    Add files via upload

--- Css8bai6.cpp
+++ Css8bai6.cpp
@@ -4,9 +4,9 @@ int main(){
 	int sum;
 	for(int i=0;i<3;i++){
 	    for(int j=0;j<3;j++){
-	    		sum+=matrix[i][j];
 	    		if(i==j){
 	    			printf("%d",matrix[i][j]);
+	    			sum+=matrix[i][j];
 				}else{
 					printf(" ");
 				}
